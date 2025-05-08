e7baa7874497feff9497ccaf66cab3c28861efe4
JesryleAgrabio <kitsunemachi097@gmail.com>
2025-05-09 05:54:54 +0800

    Changed some codes

--- Pharmacy/Dashboard.php
+++ Pharmacy/Dashboard.php
@@ -32,7 +32,7 @@ $res = $query->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JEEP Moderator Dashboard</title>
+    <title>Dashboard</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
